f31d816d8027662dd124acdb70c02c190660d175
toririm <longtimedragon@icloud.com>
2024-01-02 14:33:03 +0900

    update

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
   LiveReload,
@@ -9,25 +8,27 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "~/styles/global.css";
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  {rel: 'stylesheet', href: styles },
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <main role="application">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-      </body>
+      </main>
     </html>
   );
 }
