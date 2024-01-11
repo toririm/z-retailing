8fb8eee8c0ad7e8c944af84ad40d628c370fdd21
toririm <longtimedragon@icloud.com>
2024-01-11 13:20:34 +0900

    add: setup page

--- app/routes/setup.tsx
+++ app/routes/setup.tsx
@@ -1,7 +1,9 @@
-import { Form, redirect } from "@remix-run/react";
+import { Form, redirect, useActionData } from "@remix-run/react";
 import { badRequest } from "~/utils/request.server";
 import type { ActionFunctionArgs } from "@remix-run/cloudflare";
 import { supabaseClient } from "~/supabase.server";
+import { getSession } from "~/utils/session.server";
+import { prismaClient } from "~/utils/prisma.server";
 
 export const action = async ({ context, request }: ActionFunctionArgs) => {
   const form = await request.formData();
@@ -12,15 +14,43 @@ export const action = async ({ context, request }: ActionFunctionArgs) => {
       errorMsg: "フォームが正しく送信されませんでした",
     });
   }
-  // todo: create user record
+  const supabase = supabaseClient(context);
+  const session = await getSession(request.headers.get("Cookie"));
+  const {
+    data: { user },
+    error
+  } = await supabase.auth.getUser(
+    session.get("access_token")
+  );
+  const prisma = prismaClient(context);
+  if (!user) {
+    return badRequest({
+      nickname: "",
+      errorMsg: "ログインが失敗しています",
+    });
+  }
+  if (!user.email) {
+    return badRequest({
+      nickname: "",
+      errorMsg: "ログイン不正:メールドレスが登録されていません",
+    });
+  }
+  const data = await prisma.user.create({
+    data: {
+      authId: user.id,
+      name: nickname,
+      email: user.email,
+    },
+  });
   return redirect("/user");
 }
 
 export default function Setup() {
+  const actionData = useActionData<typeof action>();
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="card bg-base-100 shadow-xl">
-        <Form method="post" className="card-body items-center text-center">
+        <Form method="post" className="card-body items-center">
           <h2 className="card-title">ニックネームを入れよう！</h2>
           <label className="from-control">
             <div className="label">
@@ -35,6 +65,16 @@ export default function Setup() {
               required
             />
           </label>
+          <div className="label">
+            {actionData?.errorMsg ? (
+              <span
+                className="label-text-alt text-error"
+                role="alert"
+              >
+                {actionData.errorMsg}
+              </span>
+            ) : null}
+          </div>
           <div className="card-actions p-3">
             <button type="submit" className="btn btn-wide btn-info">登録</button>
           </div>
