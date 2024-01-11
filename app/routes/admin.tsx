aea57fa3a571ac2713d0d39e20c6536679998bce
toririm <longtimedragon@icloud.com>
2024-01-11 18:21:58 +0900

    add: admin route

---
+++ app/routes/admin.tsx
@@ -0,0 +1,33 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/cloudflare";
+import { Outlet } from "@remix-run/react";
+import { getUser } from "~/supabase.server"
+import { prismaClient } from "~/utils/prisma.server";
+
+export const loader = async ({ context, request }: LoaderFunctionArgs) => {
+  const authUser = await getUser(context, request);
+  if (!authUser) {
+    return redirect("/login");
+  }
+  const prisma = prismaClient(context);
+  const user = await prisma.user.findUnique({
+    where: {
+      authId: authUser.id,
+      admin: true,
+    },
+  });
+  if (!user) {
+    return redirect("/user");
+  }
+  return user;
+}
+
+export default function UserRoute() {
+  return (
+    <>
+      <nav className="navbar bg-base-100">
+        <h1 className="btn btn-ghost text-xl">Ｚ物販 管理者ページ</h1>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
