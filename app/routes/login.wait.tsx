b1347f0373b576320ee9fa30f76f0b9ebcf2653a
toririm <longtimedragon@icloud.com>
2024-01-14 18:33:04 +0900

    fix: styles

--- app/routes/login.wait.tsx
+++ app/routes/login.wait.tsx
@@ -4,5 +4,16 @@ export const meta = () => [
 ];
 
 export default function Wait() {
-  return <h2>メールを確認してください。</h2>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="card card-bordered bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">認証メールを確認してください</h2>
+          <p className="text-base-content">
+            メールが届かない場合は、迷惑メールフォルダを確認してください。
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
